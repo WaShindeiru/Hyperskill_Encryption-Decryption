c696a635593047be1373afbbdcffcda9288d7627
WaShindeiru <franekdata@gmail.com>
2024-05-24 18:23:36 +0200

    add Uppercase letters

--- src/main/java/encryptdecrypt/EncryptionShift.java
+++ src/main/java/encryptdecrypt/EncryptionShift.java
@@ -4,7 +4,11 @@ public class EncryptionShift implements IEncrypt{
 
     public static int a_code = 97;
     public static int z_code = 122;
-    public static int code_distance = 122 - 97 + 1;
+    public static int code_distance = a_code - z_code + 1;
+
+    public static int A_code = 65;
+    public static int Z_code = 90;
+    public static int uppercase_code_distance = A_code - Z_code + 1;
 
     @Override
     public String encrypt(String toEncrypt, int key) {
@@ -15,13 +19,24 @@ public class EncryptionShift implements IEncrypt{
             char currentChar = toEncrypt.charAt(i);
 
             if (Character.isAlphabetic(currentChar)) {
-                int newCode = (int) (currentChar + key);
-                if (newCode > z_code) {
-                    newCode = newCode - code_distance;
-                }
+                if (Character.isLowerCase(currentChar)) {
+                    int newCode = (int) (currentChar + key);
+                    if (newCode > z_code) {
+                        newCode = newCode - code_distance;
+                    }
 
-                char newChar = (char) (newCode);
-                encrypted.append(newChar);
+                    char newChar = (char) (newCode);
+                    encrypted.append(newChar);
+
+                } else {
+                    int newCode = (int) (currentChar + key);
+                    if (newCode > Z_code) {
+                        newCode = newCode - uppercase_code_distance;
+                    }
+
+                    char newChar = (char) (newCode);
+                    encrypted.append(newChar);
+                }
 
             } else {
                 encrypted.append(currentChar);
@@ -40,13 +55,25 @@ public class EncryptionShift implements IEncrypt{
             char currentChar = toEncrypt.charAt(i);
 
             if (Character.isAlphabetic(currentChar)) {
-                int newCode = (int) (currentChar - key);
-                if (newCode < a_code) {
-                    newCode = newCode + code_distance;
-                }
+                if (Character.isLowerCase(currentChar)) {
+                    int newCode = (int) (currentChar - key);
+                    if (newCode < a_code) {
+                        newCode = newCode + code_distance;
+                    }
 
-                char newChar = (char) (newCode);
-                encrypted.append(newChar);
+                    char newChar = (char) (newCode);
+                    encrypted.append(newChar);
+
+                } else {
+                    int newCode = (int) (currentChar - key);
+                    if (newCode < A_code) {
+                        newCode = newCode + uppercase_code_distance;
+                    }
+
+                    char newChar = (char) (newCode);
+                    encrypted.append(newChar);
+
+                }
 
             } else {
                 encrypted.append(currentChar);
